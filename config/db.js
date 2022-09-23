cfa03bb4116fda084a80a3012d8b4f3e2e2f4c5f
zoker <flaviogabriel2603@gmail.com>
2022-09-23 13:43:44 -0300

    att

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: ''}
+    module.exports = {mongoURI: 'mongodb+srv://zoker:oYt5uhgLhODRJtN8@videostreaming.ji2fqcy.mongodb.net/?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/video-streaming'}
 }
